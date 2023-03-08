e4d2bdd1e637236417136f2d05ee49d643b2f0d7
mengyunlei <mengyunlei@ppdai.com>
2023-03-09 07:39:07 +0800

    first commit

---
+++ xxlgood/src/main/java/com/xl/code/study/no_5/bfpp/ReplaceObscenitiesBeanFactoryPostProcessor.java
@@ -0,0 +1,27 @@
+package com.xl.code.study.no_5.bfpp;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+
+/**
+ * ClassName: ReplaceRubbishWordBeanFactoryPostProcessor
+ * Description:
+ * date: 2020/10/19 17:53
+ *
+ * @author 小刘讲师，微信：vv517956494
+ * 本课程属于 小刘讲师 VIP 源码特训班课程
+ * 严禁非法盗用（如有发现非法盗取行为，必将追究法律责任）
+ * <p>
+ * 如有同学发现非 小刘讲源码 官方号传播本视频资源，请联系我！
+ * @since 1.0.0
+ */
+public class ReplaceRubbishWordBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
+
+	private Set<String> 
+
+	@Override
+	public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+
+	}
+}
