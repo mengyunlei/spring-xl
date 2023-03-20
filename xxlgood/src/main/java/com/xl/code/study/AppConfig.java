b12346d9ae006e350a0e744abfcae0e7181e7b71
mengyunlei <374885871@qq.com>
2023-03-20 08:58:04 +0800

    fgh

--- xxlgood/src/main/java/com/xl/code/study/AppConfig.java
+++ xxlgood/src/main/java/com/xl/code/study/AppConfig.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Component;
 
 @ComponentScan("com.xl.code.study")
 @EnableAspectJAutoProxy
+
 public class AppConfig {
 
 
--- xxlgood/src/main/java/com/xl/code/study/myl/BService.java
+++ xxlgood/src/main/java/com/xl/code/study/myl/BService.java
@@ -3,7 +3,6 @@ package com.xl.code.study.myl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.Resource;
 
 @Component
 public class BService {
